3c57a3645d5b53f62fcfaad6d5068a2ff014a09a
nyarvan <90792424+nyarvan@users.noreply.github.com>
2022-11-22 14:52:25 +0200

    Update settings.py

--- MersHub/settings.py
+++ MersHub/settings.py
@@ -54,7 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'MersHub.urls'

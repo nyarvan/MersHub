cf8bd12479fbf4ac350c07ec72707e4bc8893692
nyarvan <90792424+nyarvan@users.noreply.github.com>
2022-11-22 14:27:18 +0200

    Update settings.py

--- MersHub/settings.py
+++ MersHub/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ye!mn#stw4(71##$-74!a9yx(e9^)m8b@qny(nvl3&*=yb$&*n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

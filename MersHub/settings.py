1d352cb847284a211acce3280a3eb0a5ba295e30
Ivan <infinity.man102@gmail.com>
2022-11-26 00:06:03 +0200

    Add robots and sitemap

--- MersHub/settings.py
+++ MersHub/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'robots',
     'django.contrib.sitemaps',
     'storages',
     'shop.apps.ShopConfig',

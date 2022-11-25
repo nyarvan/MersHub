ef9944baa516f9c803694482230623d5eab82496
Ivan <infinity.man102@gmail.com>
2022-11-26 00:03:41 +0200

    Add robots and sitemap

--- MersHub/urls.py
+++ MersHub/urls.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import GenericSitemap
+from django.views.generic.base import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 import debug_toolbar
+from shop.models import Product
+
+info_dict = {
+    'queryset': Product.objects.all(),
+    'date_field': 'created',
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls', namespace='shop')),
     path('cart/', include('cart.urls', namespace='cart')),
-    path('order/', include('order.urls', namespace='order'))
+    path('order/', include('order.urls', namespace='order')),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
+    path('sitemap.xml', sitemap,
+         {'sitemaps': {'blog': GenericSitemap(info_dict, priority=0.6)}},
+         name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 if settings.DEBUG:

fdea67c27b1e09dd8d18facf6f82935e329bdd58
Ivan <infinity.man102@gmail.com>
2022-12-08 11:57:10 +0200

    Change sitemaps settings

--- MersHub/sitemaps.py
+++ MersHub/sitemaps.py
@@ -7,7 +7,7 @@ class StaticViewSitemap(sitemaps.Sitemap):
     changefreq = 'daily'
 
     def items(self):
-        return ['shop.homepage', 'shop.contact', 'shop.about-us', 'shop.delivery-payment', 'shop.guarantee']
+        return ['homepage', 'contact', 'about-us', 'delivery-payment', 'guarantee']
 
     def location(self, item):
         return reverse(item)
